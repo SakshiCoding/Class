25532e079ab91edcc87ea0391112df3dc33a13e0
SakshiCoding <54544639+SakshiCoding@users.noreply.github.com>
2020-11-26 17:49:06 -0800

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,23 @@
+class Ground{
+constructor(){
+
+    var options = {
+        isStatic: true
+    } 
+    this.body= Bodies.rectangle(200, 390, 400, 10, options)
+    World.add(world, this.body)
+}
+
+display(){
+    var pos = this.body.position
+    rectMode(CENTER);
+    fill ("purple");
+    rect(pos.x, pos.y, 400, 10);
+}
+
+
+
+
+
+
+}
